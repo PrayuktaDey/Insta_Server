c6500f1a1cf4f1fbd1b79bcfb6939161ca12bd61
Prayukta Dey <iamdmimi2000@gmail.com>
2021-08-07 11:56:27 +0530

    insta_server

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// module.exports ={
+//     // MONGOURI:"mongodb+srv://Prayukta:mimidey@insta-clone.eyhqg.mongodb.net/test",
+//     // JWT_SECRET:"jjjherechutdwsfg",
+//     SENDGRID_API:"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"
+// }
+
+
+if(process.env.NODE_ENV==='production'){
+    module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+}
